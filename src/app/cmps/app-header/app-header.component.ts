4ab149cab434b8338ff24dc1b89e38497ed90e95
Tomer Appelman <toap012@gmail.com>
2023-08-27 12:55:36 +0300

    add unsaved changes

---
+++ src/app/cmps/app-header/app-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'AppHeader',
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.scss']
+})
+export class AppHeaderComponent {
+}
